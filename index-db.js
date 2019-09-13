d8e630188b9a19323edcc9c863024123e9271adf
tricaoktek <55221797+tricaoktek@users.noreply.github.com>
2019-09-13 10:32:34 +0700

    Update index-db.js

--- index-db.js
+++ index-db.js
@@ -4,6 +4,7 @@ var mysql = require("mysql");
 var con = mysql.createConnection({ host: process.env.MYSQL_HOST, user: process.env.MYSQL_USER, password: process.env.MYSQL_PASSWORD, database: process.env.MYSQL_DATABASE});
 
 // mysql code
+// test comment
 
 con.connect(function(err){
   if(err){
