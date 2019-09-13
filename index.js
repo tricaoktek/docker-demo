8940915c12cc4489f6103b8ccb0f7a0b8efa54e5
tricaoktek <55221797+tricaoktek@users.noreply.github.com>
2019-09-13 15:21:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// test demo
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
